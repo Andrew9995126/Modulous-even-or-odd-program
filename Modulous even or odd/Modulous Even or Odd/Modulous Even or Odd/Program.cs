c9d8a77a2aa57d729112db68c5f1f22c3f55e955
Andrew Arther <9995126@student.toiohomai.ac.nz>
2019-07-31 11:44:03 +1200

    commit 1

---
+++ Modulous even or odd/Modulous Even or Odd/Modulous Even or Odd/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modulous_Even_or_Odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            if (num1 % 2==0)
+            { Console.WriteLine("Your number is even!");
+            }
+
+            else
+                { Console.WriteLine("Your number is odd!");
+            }
+
+            Console.ReadLine();
+                
+        }
+    }
+}
